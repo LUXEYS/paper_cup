135fd2a3b4d097b637c463df68383d8698be4b1b
tessier <tessier@luxeys.com>
2020-08-25 10:04:17 +0900

    [Refactor] test structure

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 build:
 	python setup.py sdist
 test:
-	python3 -m unittest paper_cup.test
+	python3 -m unittest
 package_upload_test:
 	twine upload --verbose --repository-url https://test.pypi.org/legacy/ dist/*
 package_upload:
--- paper_cup/test/test_paper_cup.py
+++ paper_cup/test/test_paper_cup.py
@@ -5,25 +5,6 @@ from paper_cup.client import SNSClient, SQSClient
 from paper_cup.paper_cup import PaperCup
 
 
-class TestSNSClient(TestCase):
-
-  def test_instance(self):
-    """Check that the instance initialize correctly."""
-    sns_client = SNSClient()
-    # check that we correctly set an sns session
-    self.assertTrue(sns_client.sns)
-
-
-class TestSQSClient(TestCase):
-
-  def test_instance(self):
-    """Check that the instance initialize correctly."""
-    sqs_client = SQSClient()
-    # check that we correctly set an sqs session
-    self.assertTrue(sqs_client.sqs)
-    self.assertTrue(sqs_client.sqs_obj)
-
-
 class TestPaperCup(TestCase):
 
   def setUp(self):
